a19bc72430f39c734a41e1b7e778e4a501382b79
projess <j.khamsoi@gmail.com>
2021-02-12 13:58:42 -0800

    to do list

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { useState } from "react";
+import './app.css'
+const App = ()=>
+{
+    const [toDoList, setToDoList] = useState([]);
+    const [newItem, setNewItem] = useState("");
+    const listItems = toDoList.map((toDoItem) =>
+    <li for="a"><input type="checkbox" id="a"/>{toDoItem}
+    </li>);
+    const addItem =(e)=>
+    {
+        e.preventDefault();
+        if(newItem !== "")
+        {
+            const newList = toDoList.concat(newItem);
+             setToDoList(newList);
+             setNewItem("");
+            document.forms['form_id'].reset();
+        }
+     
+    }
+    return(
+      <div class="flex-container">
+          <div class="flex-one">
+              <h1>To Do List</h1>
+              <ul>{listItems}</ul>
+          </div>
+          <div class="flex-two">
+            <form id = "form_id" onSubmit ={addItem}>
+                <input type="text" onChange={(e)=>setNewItem(e.target.value)}/>
+                <br/><br/><br/>
+                <input type="submit" value = "add item"/>
+            </form>
+          </div>
+          
+     </div>
+    )
 }
-
 export default App;
